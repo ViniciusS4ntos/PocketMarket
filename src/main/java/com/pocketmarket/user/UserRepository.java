a14c5e67a9039c4af6294477e150b09218594a09
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2026-05-21 16:17:23 -0300

    fix: added annotation @Repository in UserRepository

--- src/main/java/com/pocketmarket/user/UserRepository.java
+++ src/main/java/com/pocketmarket/user/UserRepository.java
@@ -1,9 +1,11 @@
 package com.pocketmarket.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-import java.util.UUID;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
